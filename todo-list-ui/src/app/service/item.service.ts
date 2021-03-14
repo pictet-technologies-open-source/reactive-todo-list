71b0fece5276ed3aec57c3f0d3260074a9f3b3dc
Alexandre Jacquot <ajacquot@pictet-technologies.com>
2021-03-14 14:09:44 +0100

    remove not needed take operator

--- todo-list-ui/src/app/service/item.service.ts
+++ todo-list-ui/src/app/service/item.service.ts
@@ -26,32 +26,27 @@ export class ItemService {
   }
 
   findAll(): Observable<Item[]> {
-    return this.http.get<Item[]>(this.baseUrl).pipe(take(1));
+    return this.http.get<Item[]>(this.baseUrl);
   }
 
   addItem(description: string): Observable<any> {
-    return this.http.post<Item>(this.baseUrl, {description})
-      .pipe(take(1));
+    return this.http.post<Item>(this.baseUrl, {description});
   }
 
   findById(id: string): Observable<Item> {
-    return this.http.get<Item>(`${this.baseUrl}/${id}`)
-      .pipe(take(1));
+    return this.http.get<Item>(`${this.baseUrl}/${id}`);
   }
 
   delete(id: string, version: number): Observable<any> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`)
-      .pipe(take(1));
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
   }
 
   updateDescription(id: string, version: number, description: string): Observable<any> {
-    return this.http.patch<void>(`${this.baseUrl}/${id}`, {description})
-      .pipe(take(1));
+    return this.http.patch<void>(`${this.baseUrl}/${id}`, {description});
   }
 
   updateStatus(id: string, version: number, status: ItemStatus): Observable<any> {
-    return this.http.patch<void>(`${this.baseUrl}/${id}`, {status})
-      .pipe(take(1));
+    return this.http.patch<void>(`${this.baseUrl}/${id}`, {status});
   }
 
 }
